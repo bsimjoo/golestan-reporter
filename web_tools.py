41026a4ed94e7b8302f8d8a3fc4a5564ceef46a8
Farooq Karimi Zadeh <hapu@riseup.net>
2020-09-29 06:56:17 +0000

    shorter (better?) code

--- web_tools.py
+++ web_tools.py
@@ -60,8 +60,7 @@ class root:
 
     def get_random_string(self,length):
         letters = string.ascii_lowercase+string.digits+string.ascii_uppercase
-        result_str = ''.join(random.choice(letters) for i in range(length))
-        return result_str
+        return random.choices(letters, k=length)
 
     def expire_key(self,enum,index):
         del enum[index]
